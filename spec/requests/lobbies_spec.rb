aae403b28ecf114f8283381b97130586ec73b3d6
Igor Pavlov <gophan1992@gmail.com>
2019-01-07 16:12:53 +0300

    fixed request spec

--- spec/requests/lobbies_spec.rb
+++ spec/requests/lobbies_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Lobbies", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe "GET /lobbies" do
     it "works! (now write some real specs)" do
-      get lobbies_path
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
+
+      get lobbies_path, headers: auth_headers
       expect(response).to have_http_status(200)
     end
   end
